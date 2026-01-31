9b8634d6e782230673db5625993d936b1af69ce2
Hugo Soares <hugos2000@protonmail.com>
2026-01-31 16:14:57 -0300

    added pps mechanic

--- scripts/truck.gd
+++ scripts/truck.gd
@@ -2,9 +2,15 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var points_label: Label = $"../PointsLabel"
+@onready var pps_label: Label = $"../PPSLabel"
 
 func _ready() -> void:
 	anim.play()
+
+func _process(delta: float) -> void:
+	Global.points += Global.points_per_second * delta
+	points_label.text = "%d Points" % int(Global.points)
+	pps_label.text = str(Global.points_per_second) + " Points/Second"
 
 func _input_event(_viewport, event, _shape_idx) -> void:
 	if event is InputEventMouseButton \
@@ -12,4 +18,3 @@ func _input_event(_viewport, event, _shape_idx) -> void:
 	and event.pressed:
 		Global.points += 1
 		print(Global.points)
-		points_label.text = str(Global.points) + " Points"
