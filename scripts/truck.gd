671ac5f9a4497b07ed0ed8427a9614246b3289e9
Hugo Soares <hugos2000@protonmail.com>
2026-01-31 15:30:51 -0300

    added basic clicker mechanic

--- scripts/truck.gd
+++ scripts/truck.gd
@@ -4,3 +4,10 @@ extends CharacterBody2D
 
 func _ready() -> void:
 	anim.play()
+
+func _input_event(_viewport, event, _shape_idx) -> void:
+	if event is InputEventMouseButton \
+	and event.button_index == MOUSE_BUTTON_LEFT \
+	and event.pressed:
+		Global.points += 1
+		print(Global.points)
