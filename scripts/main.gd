b252455f65fae86eccb9670cc6cc003e6aa6ad37
Hugo Soares <hugos2000@protonmail.com>
2026-01-31 17:03:28 -0300

    added character placeholder

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 @onready var points_label: Label = $PointsLabel
 
-const UPGRADE_COST := 50
+@onready var kwid: AnimatableBody2D = $kwid
+
+const UPGRADE_COST := 10
 
 func _ready() -> void:
 	update_points_label()
+	kwid.visible = false
 
 func _on_texture_button_pressed() -> void:
 	if Global.points >= UPGRADE_COST:
 		Global.points -= UPGRADE_COST
 		Global.points_per_second += 1.0
 		Global.points_per_click += 1.0
+		kwid.visible = true
 		update_points_label()
 
 func update_points_label() -> void:
