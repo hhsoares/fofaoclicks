50941b3880aa108f6feedb9fe136b961e2e95c42
Hugo Soares <hugos2000@protonmail.com>
2026-02-01 12:56:42 -0300

    added labels

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 @onready var points_label: Label = $PointsLabel
+@onready var truck_label: Label = $Panel/TruckLabel
+@onready var fofao_label: Label = $Panel/FofaoLabel
 
 @onready var fofao: Node2D = $Fofao
 
 const FOFAO_UPGRADE_COST := 10
 const TRUCK_UPGRADE_COST := 20
+
+var truck_level = 1
+var fofao_level = 0
 
 func _ready() -> void:
 	update_points_label()
@@ -21,6 +26,8 @@ func _on_fofao_button_pressed() -> void:
 		Global.points_per_click += 1.0
 		fofao.visible = true
 		fofao.get_node("AnimatedSprite2D").play("default")
+		fofao_level += 1
+		fofao_label.text = "Fofão level %d" % fofao_level
 		update_points_label()
 
 func _on_truck_button_pressed() -> void:
@@ -28,4 +35,6 @@ func _on_truck_button_pressed() -> void:
 		Global.points -= TRUCK_UPGRADE_COST
 		Global.points_per_second += 1.0
 		Global.points_per_click += 1.0
+		truck_level += 1
+		truck_label.text = "Truck level %d" % truck_level
 		update_points_label()
