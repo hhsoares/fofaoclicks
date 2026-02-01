f3dbb81b9cfb13514df403d57866b3238c4bc446
Hugo Soares <hugos2000@protonmail.com>
2026-02-01 12:40:00 -0300

    added truck upgrade

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,22 +2,30 @@ extends Node2D
 
 @onready var points_label: Label = $PointsLabel
 
-@onready var kwid: Node2D = $kwid
+@onready var fofao: Node2D = $Fofao
 
-const UPGRADE_COST := 10
+const FOFAO_UPGRADE_COST := 10
+const TRUCK_UPGRADE_COST := 20
 
 func _ready() -> void:
 	update_points_label()
-	kwid.visible = false
-
-func _on_texture_button_pressed() -> void:
-	if Global.points >= UPGRADE_COST:
-		Global.points -= UPGRADE_COST
-		Global.points_per_second += 1.0
-		Global.points_per_click += 1.0
-		kwid.visible = true
-		kwid.get_node("AnimatedSprite2D").play("default")
-		update_points_label()
+	fofao.visible = false
 
 func update_points_label() -> void:
 	points_label.text = "%d Points" % int(Global.points)
+
+func _on_fofao_button_pressed() -> void:
+	if Global.points >= FOFAO_UPGRADE_COST:
+		Global.points -= FOFAO_UPGRADE_COST
+		Global.points_per_second += 1.0
+		Global.points_per_click += 1.0
+		fofao.visible = true
+		fofao.get_node("AnimatedSprite2D").play("default")
+		update_points_label()
+
+func _on_truck_button_pressed() -> void:
+	if Global.points >= TRUCK_UPGRADE_COST:
+		Global.points -= TRUCK_UPGRADE_COST
+		Global.points_per_second += 1.0
+		Global.points_per_click += 1.0
+		update_points_label()
